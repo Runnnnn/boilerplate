d22bf64125962a3e66461ee845b9a4d67c09e8f0
Runnnnn <349101727@qq.com>
2017-08-15 15:28:33 +0800

    first commit

---
+++ webpack.development.js
@@ -0,0 +1,49 @@
+const path = require('path');
+const webpack = require('webpack');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+console.log('--------------development--------------')
+module.exports = {
+    entry: {
+        //If there are any duplicated modules between entry chunks they will be included in both bundles.
+        app: './src/index.js',
+    },
+    devtool: 'source-map',
+    devServer: {
+        hot: true,
+        contentBase: path.join(__dirname, 'src'),
+        publicPath: '/',
+    },
+    output: {
+        filename: '[name].[hash].js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin(),
+        new HtmlWebpackPlugin({title: 'template', template: './src/assets/index.html', inject: false}),
+        new webpack.DefinePlugin({
+            'process.env': {
+                'NODE_ENV': JSON.stringify('development')
+             }
+        }),
+        new webpack.optimize.CommonsChunkPlugin({
+            name: 'common'
+        }),
+        new ExtractTextPlugin("styles.[hash].css"),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ExtractTextPlugin.extract({fallback: "style-loader", use: "css-loader"})
+            }, {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: ['file-loader']
+            }, {
+                test: /\.js$/, // babel 转换为兼容性的 js
+                exclude: /node_modules/,
+                loader: 'babel-loader'
+            }
+        ]
+    }
+};
---
+++ webpack.production.js
@@ -0,0 +1,43 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+console.log('--------------production--------------')
+module.exports = {
+    entry: {
+        app: './src/index.js',
+    },
+    devtool: 'inline-source-map',
+    output: {
+        filename: '[name].[hash].js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    plugins: [
+        new CleanWebpackPlugin(['dist']),
+        new HtmlWebpackPlugin({title: 'template', template: './src/assets/index.html', inject: false}),
+        new webpack.optimize.UglifyJsPlugin({sourceMap: true}),
+        new webpack.DefinePlugin({
+            'process.env': {
+                'NODE_ENV': JSON.stringify('production')
+            }
+        }),
+        new webpack.optimize.CommonsChunkPlugin({name: 'common'}),
+        new ExtractTextPlugin("styles.[hash].css")
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ExtractTextPlugin.extract({fallback: "style-loader", use: "css-loader"})
+            }, {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: ['file-loader']
+            }, {
+                test: /\.js$/, // babel 转换为兼容性的 js
+                exclude: /node_modules/,
+                loader: 'babel-loader'
+            }
+        ]
+    }
+};
